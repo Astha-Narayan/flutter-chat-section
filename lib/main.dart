dbaa52827199873381a64f12b2e4ec9dc00b5a28
Astha Narayan <astha101200@gmail.com>
2022-06-09 18:58:22 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:chatapp/helper/helperfunctions.dart';
 import 'package:chatapp/views/chatrooms.dart';
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MyApp());
 }
